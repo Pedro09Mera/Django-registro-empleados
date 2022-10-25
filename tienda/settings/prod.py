7e001ebcd5d30c6f6371e32fbe52cfd5d5277af0
Pedro09Mera <merap697@gmail.com>
2022-10-25 18:12:35 -0500

    cambios

--- tienda/settings/prod.py
+++ tienda/settings/prod.py
@@ -1,5 +1,5 @@
 from .base import *
-# SECURITY WARNING: don't run with debug turned on in production!
+## SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['159.223.115.154']
